94ddb5bf9a6bbe8f121814e236ce7aa39c76ee45
Samina Trachier <st3225@nyu.edu>
2020-04-11 17:35:30 -0400

    Added Fill Bar and Fixed Interactions Colliders for Game Manager

---
+++ Assets/Scripts/Collisions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Hit");
+        //passing gameObject that player collided with in Cornwash Function
+        GameManager.instance.CornWash(col.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 //USAGE: GAME MANAGER- tracks player's mouse movements for Swipe and Tap Actions for minigames
 public class GameManager : MonoBehaviour
 {
     public GameObject mousePosition;
+
+    public static GameManager instance;
+    
+    //progress bar Image
+    public Image progressBar;
+
+    //progress tracker
+    public float progress;
+    private float maxProgress = 1;
+
+    private GameObject lastHitObject;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //to reference the entire script on collisions
+        instance = this;
     }
 
     // Update is called once per frame
+    
+    //establish mouse movement relative to screen
     void Update()
     {
+        //mousePosition.transform.position = new Vector3(worldPosition.x, worldPosition.y, 0);
         //establish world position of the mouse
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       // Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       // mousePosition.transform.position = new Vector3(worldPosition.x, worldPosition.y, 0);
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+       Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
         mousePosition.transform.position = worldPosition;
+        
+        //lerps(makes the fill smooth for the progressBar)(if it needs to be faster multiply Time.deltaTime by multiplier of choice)
+        //progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount,progress,Time.deltaTime);
+        
+        //winstate:
+        if (progress >= maxProgress)
+        {
+            //move to next minigame OR end module and go back to map/results screen
+        }
 
+
+    }
+
+    //corn washing minigame
+    public void CornWash(GameObject hitObject)
+    {
+        if (lastHitObject != hitObject)
+        {
+            //increases the progress bar
+            progress += .01f;
+            
+            
+            //for testing purposes
+            progressBar.fillAmount = progress;
+            
+            //last hit Object
+            lastHitObject = hitObject;
+
+        }
     }
 }
