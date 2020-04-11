5c39e460a02f781b059fbf7392a083f984743e55
Samina Trachier <st3225@nyu.edu>
2020-04-11 16:41:44 -0400

    Added Colliders and Game Manager-> Mouse Position

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//USAGE: GAME MANAGER- tracks player's mouse movements for Swipe and Tap Actions for minigames
+public class GameManager : MonoBehaviour
+{
+    public GameObject mousePosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //establish world position of the mouse
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.transform.position = worldPosition;
+
+    }
+}
